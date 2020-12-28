ebc24867226f116d4c2b96f4b6998ee23467d4e2
PavelPodolnitsky <76696511+PavelPodolnitsky@users.noreply.github.com>
2020-12-28 20:53:20 +0300

    1

---
+++ scripts/script.js
@@ -0,0 +1,23 @@
+const a = 'stroka 2';
+let b = 10;
+const c = 25; 
+console.log(a+b);
+b++;
+alert(`privet
+4
+ "mir"
+ ${c%b}`);
+  let i = {
+      f:2,
+      g:'pirogov',
+      p: true ,
+      q:{
+        y: 2,
+        m: 3,     
+    },
+    z:[0,1,2,3,4,5],
+  };
+  console.log(i);
+ 
+
+  
